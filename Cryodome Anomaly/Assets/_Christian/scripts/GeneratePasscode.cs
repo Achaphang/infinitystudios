75c13804c3b66749bad35fe844fc3b53d4fd9f2d
Christian <oceanwhit@gmail.com>
2020-02-26 22:55:59 -0800

    Make generatepasscode

---
+++ Cryodome Anomaly/Assets/_Christian/scripts/GeneratePasscode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random;
+
+public class GeneratePasscode : MonoBehaviour
+{
+    int[] passcode = new int[4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random rng = new Random();
+        for (int i = 0; i < 4; i ++) {
+            passcode[i] = rng.Next(0, 9);
+        }
+    }
+}
