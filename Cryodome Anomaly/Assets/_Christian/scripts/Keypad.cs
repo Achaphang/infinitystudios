34033bb5580bf810a83aa8e24ca83d418a695ad7
Yamist <8bityamist@gmail.com>
2020-03-02 19:40:07 -0800

    incorporated keypad generation, monster patroling

--- Cryodome Anomaly/Assets/_Christian/scripts/Keypad.cs
+++ Cryodome Anomaly/Assets/_Christian/scripts/Keypad.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Keypad : MonoBehaviour
 {
     int[] passcode = new int[4];
+    Text txt;
 
     // Start is called before the first frame update
     void Start()
@@ -12,5 +14,8 @@ public class Keypad : MonoBehaviour
         for (int i = 0; i < 4; i ++) {
             passcode[i] = Random.Range(0, 9);
         }
+
+        txt = GetComponentInChildren<Text>();
+        txt.text = string.Join("", passcode);
     }
 }
--- Cryodome Anomaly/Assets/_Jon/scripts/MonsterController.cs
+++ Cryodome Anomaly/Assets/_Jon/scripts/MonsterController.cs
@@ -5,9 +5,9 @@ using UnityEngine.AI;
 
 public class MonsterController : MonoBehaviour
 {
-    List<Transform> targets;
+    List<Vector3> targets;
     // This object is a square box that acts as a target collider for the monster.
-    GameObject markerPrefab;
+    public GameObject markerPrefab;
     NavMeshAgent agent;
 
     // For testing purposes.
@@ -21,12 +21,14 @@ public class MonsterController : MonoBehaviour
 
     void Start() {
         // Generates list, must call constructor
-        targets = new List<Transform>();
-        markerPrefab = Resources.Load("assets/prefabs/markerPrefab") as GameObject;
+        targets = new List<Vector3>();
+        // Marker prefab temporary solution
+        //markerPrefab = Resources.Load("assets/prefabs/markerPrefab") as GameObject;
         agent = GetComponent<NavMeshAgent>();
-        targets.Add(exampleTransform);
+        //targets.Add(exampleTransform);
         walkSpeed = agent.speed;
         runSpeed = walkSpeed * 2;
+        GenerateRandomTarget();
     }
 
     void Update() {
@@ -34,7 +36,7 @@ public class MonsterController : MonoBehaviour
         if (targets.Count == 0)
             return;
 
-        agent.destination = targets[0].position;
+        agent.destination = targets[0];
 
         if (stamina <= 0f)
             stopRunning();
@@ -47,6 +49,26 @@ public class MonsterController : MonoBehaviour
             stamina += .05f;
     }
 
+    Vector3 GetRandomLocation() {
+        //Calculates and returns triangulation of navmesh containing vertices, triangle indices and navmesh layers
+        NavMeshTriangulation navMeshData = NavMesh.CalculateTriangulation();
+
+        // Pick the first instance of a random triangle in the nav mesh
+        int t = Random.Range(0, navMeshData.indices.Length - 3);
+
+        // Select a random point on the first instance of a random trangle on the nav mesh
+        //Set our point as a fraction inbetween indices[t] and indices[t+1] (Done using lerp)
+        Vector3 point = Vector3.Lerp(navMeshData.vertices[navMeshData.indices[t]], navMeshData.vertices[navMeshData.indices[t + 1]], Random.value);
+        //lerp our point again with 3rd indice of triangle
+        Vector3.Lerp(point, navMeshData.vertices[navMeshData.indices[t + 2]], Random.value);
+
+        return point;
+    }
+
+    void GenerateRandomTarget() {
+        AddTarget(GetRandomLocation(), 3);
+    }
+
     void stopRunning() {
         running = false;
         agent.speed = walkSpeed;
@@ -54,21 +76,24 @@ public class MonsterController : MonoBehaviour
 
     // Adds a new target with a given priority. Currently only accepts high or low priority. Generates a target to move to.
     // TODO: Allow targets to be placed as child of object, so that they can move, for example to follow the player. Currently static markers only.
-    public void AddTarget(Transform targ, int priority) {
+    public void AddTarget(Vector3 targ, int priority) {
         if (priority == 1) {
             targets.Insert(0, targ);
         } else {
             targets.Add(targ);
         }
 
-        Instantiate(markerPrefab, targ.position, Quaternion.identity);
+        Instantiate(markerPrefab, targ, Quaternion.identity);
     }
 
-    public void OnTriggerEnter2D(Collider2D collision) {
+    public void OnTriggerEnter(Collider collision) {
+        Debug.Log("AAAA");
         // Checks if the monster has reached its primary destination. If so, remove it from the list, remove target marker.
-        if(collision.gameObject.tag == "marker") {
+        if(collision.gameObject.tag == "MonsterMarker") {
             targets.RemoveAt(0);
             Destroy(collision.gameObject);
+            // TODO: Remove this
+            GenerateRandomTarget();
         }
     }
 }
