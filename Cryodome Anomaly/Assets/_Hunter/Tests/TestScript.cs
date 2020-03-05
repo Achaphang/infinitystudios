1fb9579ece97cb7d693b7b1576d7418532cca7c1
Hunter Hawkins <32747775+hunterdhawkins@users.noreply.github.com>
2020-03-04 22:02:44 -0800

    Working with individual test script

---
+++ Cryodome Anomaly/Assets/_Hunter/Tests/TestScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+
+
+namespace Tests
+{
+    public class CryoTests
+    {
+        /** Hunter's Tests
+         **/
+        [CryoTests]
+        public IEnumerator StressSpawn()
+        {
+            var time = 1 / Time.deltaTime;
+            for(int i =0; i < 100; i++)
+            {
+                time = 1 / Time.deltaTime;
+                for(int j=0; j < 100; j++)
+                {
+                    MonoBehaviour.Instantiate(Resources.Load<GameObject>("Flashlight"));
+                }
+                Debug.Log(time);
+                yield return new WaitForSeconds(1);
+                if(time < 15)
+                {
+                    Debug.Log((i + 1) * 100);
+                    if(i < 10)
+                    {
+                        Assert.Fail();
+                    }
+                    yield break;
+                }
+            }
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            //call Assert.Fail() if test fails
+            yield return null;
+        }
+
+    }
+}
