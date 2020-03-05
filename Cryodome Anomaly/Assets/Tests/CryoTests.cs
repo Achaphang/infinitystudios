47c32fdcc7d90cbfd49160d2e9cd7c3493a4a135
Achaphang <43050805+Achaphang@users.noreply.github.com>
2020-03-04 16:05:54 -0800

    UnityAutoTester
    I setup a test folder in our Unity project. That folder holds everyones tests. When you commit or push make sure to have the most recent version of the game. Also, try to make sure that you aren't committing at the same time as anybody else. If you don't have   the most recent version. Put the code you revised in a text file temporarily. Commit after you have pulled the most recent version and checked with the group.

---
+++ Cryodome Anomaly/Assets/Tests/CryoTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CryoTests
+    {
+        // Damien's Tests
+        [UnityTest]
+        public IEnumerator PlayerBoundsCheck()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+
+        // Jon's Tests
+        [UnityTest]
+        public IEnumerator MonsterBoundsCheck()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+
+        // Hunter's Tests
+        [UnityTest]
+        public IEnumerator StressSpawn()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+
+        // Tobias's Tests
+        [UnityTest]
+        public IEnumerator CryoTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+
+        // Christian's Tests
+        [UnityTest]
+        public IEnumerator RandomNumGeneratorTest()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
