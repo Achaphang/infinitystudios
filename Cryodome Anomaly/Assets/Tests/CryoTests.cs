e878400e0def9c59dd3222d53d5a1ae78fb76a8a
Yamist <8bityamist@gmail.com>
2020-03-04 22:19:44 -0800

    moving tests to main test file

--- Cryodome Anomaly/Assets/Tests/CryoTests.cs
+++ Cryodome Anomaly/Assets/Tests/CryoTests.cs
@@ -52,11 +52,46 @@ namespace Tests
             // Use yield to skip a frame.
             yield return null;
         }
+        /*
+        [UnityTest]
+        public IEnumerator SpawnMassMarkers() {
+            MonsterController monster = GameObject.Find("Monster").GetComponent<MonsterController>();
+            for (int i = 0; i < 999; i++) {
+                monster.AddTarget(new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)));
+            }
+        }
+        [UnityTest]
+        void PlayNoises() {
+            MonsterNoiseController monster = GameObject.Find("Monster").GetComponent<MonsterNoiseController>();
+            for(int i = 0; i < 999; i++) {
+                monster.commitDie();
+                monster.locatedPlayer();
+            }
+        }
+
+        // Goal: Spawn tons of markers directly on top of the monster. They should all immediately be deleted.
+        // Pass: No markers remain after a set amount of time.
+        // Fail: At least one marker is still in the scene.
+        [UnityTest]
+        void MassMarkerPlaceAtMe() {
+            MonsterController monster = GameObject.Find("Monster").GetComponent<MonsterController>();
+            int temp = monster.GetTargetCount();
+            for (int i = 0; i < 999; i++) {
+                monster.AddTarget(monster.gameObject.transform.position);
+            }
+
+            // Todo: incorporate Assert.Fail()
+            if (temp != monster.GetTargetCount())
+                return;
+                // Assert.Fail();
+        }
+        */
+        
 
         // **/
 
         /** Hunter's Tests
-         **/ 
+         **/
         [UnityTest]
         public IEnumerator StressSpawn()
         {
