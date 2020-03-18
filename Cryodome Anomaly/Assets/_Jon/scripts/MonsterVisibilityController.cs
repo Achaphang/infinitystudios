c7ccbb0184969485bafab5b8846379b97a309dcb
Yamist <8bityamist@gmail.com>
2020-03-17 17:56:28 -0700

    Final iteration of monster view hitbox, removed physical triggers.

--- Cryodome Anomaly/Assets/_Jon/scripts/MonsterVisibilityController.cs
+++ Cryodome Anomaly/Assets/_Jon/scripts/MonsterVisibilityController.cs
@@ -6,11 +6,13 @@ public class MonsterVisibilityController : MonoBehaviour
 {
     GameObject player;
     MonsterController monsterController;
-    bool canSee = false;
     float spottingTimer = 0f;
     RaycastHit hit;
 
-    // Start is called before the first frame update
+    // Used in the new vision system, angle generated a cone, distance is how far the monster can see.
+    public float viewAngle = 35f;
+    public float viewDistance = 20f;
+
     void Start()
     {
         if(GameObject.Find("3dPlayerObjs").active == true) {
@@ -24,30 +26,21 @@ public class MonsterVisibilityController : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate(){
-        if (canSee) {
-            Physics.Raycast(transform.position, (player.transform.position - transform.position), out hit);
-            if (hit.transform.tag == "ActualPlayer") {
-                spottingTimer += Time.deltaTime;
-                if(spottingTimer > .75f) {
-                    monsterController.AddTarget(player, 1);
-                    spottingTimer = 0;
+        Vector3 direction = player.transform.position - transform.position;
+
+        if(direction.magnitude <= viewDistance) {
+            if (Vector3.Angle(direction, transform.forward) < viewAngle) {
+                Physics.Raycast(transform.position, (player.transform.position - transform.position), out hit);
+                if (hit.transform.tag == "ActualPlayer") {
+                    spottingTimer += Time.deltaTime;
+                    if (spottingTimer > .75f) {
+                        monsterController.AddTarget(player, 1);
+                        spottingTimer = 0;
+                    }
+                } else {
+                    spottingTimer = 0f;
                 }
-            } else {
-                spottingTimer = 0f;
             }
-        }
-    }
-
-    // Todo: fix this function. Something in the middle of the level is triggering it, find the invisible player object. I'd guess the flashlight?
-    public void OnTriggerEnter(Collider other) {
-        if(other.gameObject.tag == "ActualPlayer") {
-            canSee = true;
-        }
-    }
-
-    public void OnTriggerExit(Collider other) {
-        if(other.gameObject.tag == "ActualPlayer") {
-            canSee = false;
         }
     }
 }
