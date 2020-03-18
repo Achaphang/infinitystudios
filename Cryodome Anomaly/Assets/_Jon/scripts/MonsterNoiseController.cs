b6292275e007a228ebe5040ea5146288a3172b97
Yamist <8bityamist@gmail.com>
2020-03-17 21:31:18 -0700

    Added monster footsteps instead of breathing

--- Cryodome Anomaly/Assets/_Jon/scripts/MonsterNoiseController.cs
+++ Cryodome Anomaly/Assets/_Jon/scripts/MonsterNoiseController.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterNoiseController : MonoBehaviour
 {
+    NavMeshAgent agent;
+    Animator anim;
     List<AudioSource> audioClips;
     AudioClip[] chasingClips;
     AudioClip[] spottedClips;
     AudioClip[] lostClips;
+    AudioClip[] movementClips;
 
     public AudioSource tempSource;
+    public AudioSource movementSource;
 
     bool playerDead = false;
     
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
         audioClips = new List<AudioSource>();
 
         foreach(AudioSource a in GetComponents<AudioSource>()) {
@@ -24,8 +31,20 @@ public class MonsterNoiseController : MonoBehaviour
         chasingClips = Resources.LoadAll<AudioClip>("Sounds/Monster Noises/chasing");
         spottedClips = Resources.LoadAll<AudioClip>("Sounds/Monster Noises/spotted");
         lostClips = Resources.LoadAll<AudioClip>("Sounds/Monster Noises/lost");
+        movementClips = Resources.LoadAll<AudioClip>("Sounds/Monster Noises/movement");
 
         StartCoroutine(breathe());
+    }
+
+    void Update() {
+        float temp = anim.GetCurrentAnimatorStateInfo(0).normalizedTime % 1;
+        if(temp > .26f && temp < .34f && agent.velocity.magnitude > .3f) {
+            move();
+        }
+
+        if (temp > .71f && temp < .75f && agent.velocity.magnitude > .3f) {
+            move();
+        }
     }
 
     IEnumerator breathe() {
@@ -37,16 +56,18 @@ public class MonsterNoiseController : MonoBehaviour
         }
     }
 
-    public void locatedPlayer() {
-        /*if (audioClips[2].isPlaying || audioClips[4].isPlaying)
-            return;
+    void move() {
+        if(!movementSource.isPlaying && !playerDead) {
+            movementSource.clip = movementClips[Random.Range(0, movementClips.Length)];
+            movementSource.pitch = 1f + agent.speed / 2f;
+            movementSource.Play();
+        }
+    }
 
-        if (Random.Range(1f, 5f) > 3f)
-            audioClips[2].Play();
-        else
-            audioClips[4].Play();*/
+    public void locatedPlayer() {
         if (!tempSource.isPlaying && !playerDead) {
             tempSource.clip = spottedClips[Random.Range(0, spottedClips.Length)];
+            tempSource.pitch = Random.Range(.7f, .8f);
             tempSource.Play();
         }
 
@@ -55,6 +76,7 @@ public class MonsterNoiseController : MonoBehaviour
     public void chasingPlayer() {
         if (!tempSource.isPlaying && !playerDead) {
             tempSource.clip = chasingClips[Random.Range(0, chasingClips.Length)];
+            tempSource.pitch = Random.Range(.7f, .8f);
             tempSource.Play();
         }
     }
@@ -62,6 +84,7 @@ public class MonsterNoiseController : MonoBehaviour
     public void lostPlayer() {
         if (!tempSource.isPlaying && !playerDead) {
             tempSource.clip = lostClips[Random.Range(0, lostClips.Length)];
+            tempSource.pitch = Random.Range(.7f, .8f);
             tempSource.Play();
         }
     }
