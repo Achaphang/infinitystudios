d90840e740754c7aed809faddbf77371e64db5b0
Yamist <8bityamist@gmail.com>
2020-03-26 16:53:56 -0700

    Added flashlight battery, fixed flashlight pose, moved scripts

---
+++ Cryodome Anomaly/Assets/_Jon/scripts/FlashlightBattery.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightBattery : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider collision) {
+        if(collision.gameObject.name == "FlashlightObject") {
+            collision.transform.GetChild(0).GetComponent<Flashlight>().RestorePower();
+            Destroy(gameObject);
+        }
+    }
+}
