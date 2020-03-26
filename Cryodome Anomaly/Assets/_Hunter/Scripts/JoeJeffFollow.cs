85177b0335868b6cf51ad81925ab614f6b8899ee
Hunter Hawkins <32747775+hunterdhawkins@users.noreply.github.com>
2020-03-26 14:40:15 -0700

    Added Joe Jeff as an ally

---
+++ Cryodome Anomaly/Assets/_Hunter/Scripts/JoeJeffFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+public class JoeJeffFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float moveSpeed;
+    NavMeshAgent joeJeff;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player.active == false)
+        {
+            player = GameObject.Find("3dPlayerObjs");
+        }
+        joeJeff = GetComponent<NavMeshAgent>();
+    }
+    private void FixedUpdate()
+    {
+        joeJeff.destination = player.transform.position+offset;
+    }
+}
