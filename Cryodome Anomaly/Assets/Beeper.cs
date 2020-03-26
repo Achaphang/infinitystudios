95dc20b68def29a05e7bee2eeb2066cdc8af2480
Yamist <8bityamist@gmail.com>
2020-03-26 15:51:35 -0700

    Improved beeper

--- Cryodome Anomaly/Assets/Beeper.cs
+++ Cryodome Anomaly/Assets/Beeper.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class Beeper : MonoBehaviour
 {
     AudioSource audio;
     AudioClip primeClip;
     AudioClip activateClip;
+    AudioClip[] explosionClips;
     MonsterController monster;
+
+    bool primed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +19,7 @@ public class Beeper : MonoBehaviour
         audio = GetComponent<AudioSource>();
         primeClip = Resources.Load<AudioClip>("Sounds/Items/Beeper/prime");
         activateClip = Resources.Load<AudioClip>("Sounds/Items/Beeper/activate");
+        explosionClips = Resources.LoadAll<AudioClip>("Sounds/Misc/Explosions");
         monster = GameObject.Find("Monster").GetComponent<MonsterController>();
     }
 
@@ -25,6 +30,9 @@ public class Beeper : MonoBehaviour
     }
 
     public void Prime() {
+        if (primed)
+            return;
+        primed = true;
         audio.clip = primeClip;
         StartCoroutine(Beep());
     }
@@ -32,17 +40,35 @@ public class Beeper : MonoBehaviour
     IEnumerator Beep() {
         for(int i = 0; i < 5; i++) {
             audio.Play();
+            monster.AddTarget(gameObject, 3);
             yield return new WaitForSeconds(.75f);
         }
-
+        audio.clip = activateClip;
+        audio.pitch = 1.75f;
         StartCoroutine(Activate());
     }
 
     IEnumerator Activate() {
-        for(int i = 0; i < 15; i++) {
+        for(int i = 0; i < 30; i++) {
             audio.Play();
             monster.AddTarget(gameObject, 2);
-            yield return new WaitForSeconds(.75f);
+            yield return new WaitForSeconds(.33f);
         }
+
+        StartCoroutine(Explode());
+    }
+
+    IEnumerator Explode() {
+        audio.clip = explosionClips[Random.Range(0, explosionClips.Length)];
+        audio.pitch = 1;
+        audio.Play();
+        transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
+
+        while (audio.isPlaying) {
+            yield return new WaitForSeconds(.1f);
+        }
+
+        Destroy(gameObject);
     }
 }
