1df9086e547e1f2671e25ddb4b56cf3fe0053ff9
Yamist <8bityamist@gmail.com>
2020-03-26 23:27:58 -0700

    Added monster prefab, added small visibilty radius around monster

--- Cryodome Anomaly/Assets/_Jon/scripts/MonsterVisibilityController.cs
+++ Cryodome Anomaly/Assets/_Jon/scripts/MonsterVisibilityController.cs
@@ -12,6 +12,7 @@ public class MonsterVisibilityController : MonoBehaviour
     // Used in the new vision system, angle generated a cone, distance is how far the monster can see.
     public float viewAngle = 45f;
     public float viewDistance = 45f;
+    public float closeViewDistance = 2f;
 
     void Start()
     {
@@ -37,6 +38,17 @@ public class MonsterVisibilityController : MonoBehaviour
                     if (spottingTimer > .2f) {
                         monsterController.AddTarget(player, 1);
                         spottingTimer = .15f;
+                    }
+                } else {
+                    spottingTimer = 0f;
+                }
+            }else if(direction.magnitude < closeViewDistance) {
+                Physics.Raycast(transform.position, (player.transform.position - transform.position), out hit);
+                if (hit.transform.tag == "ActualPlayer") {
+                    spottingTimer += Time.deltaTime;
+                    if (spottingTimer > 1f) {
+                        monsterController.AddTarget(player, 1);
+                        spottingTimer = .5f;
                     }
                 } else {
                     spottingTimer = 0f;
