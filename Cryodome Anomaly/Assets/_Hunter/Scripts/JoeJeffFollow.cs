1c5ae9c5787574ff1ec56a6080634adbe90a70d8
Hunter Hawkins <32747775+hunterdhawkins@users.noreply.github.com>
2020-03-27 10:22:56 -0700

    Added Brains and still working on JoeJeff

--- Cryodome Anomaly/Assets/_Hunter/Scripts/JoeJeffFollow.cs
+++ Cryodome Anomaly/Assets/_Hunter/Scripts/JoeJeffFollow.cs
@@ -6,24 +6,49 @@ using UnityEngine.AI;
 public class JoeJeffFollow : MonoBehaviour
 {
     public GameObject player;
-    public float moveSpeed;
-    NavMeshAgent joeJeff;
-    private Vector3 offset;
+    public LayerMask detectionLayer;
+    private Transform myTransform;
+    private NavMeshAgent myNavMeshAgent;
+    private Collider[] hitColliders;
+    private float checkRate;
+    private float nextCheck;
+    private float detectionRadius=50;
     // Start is called before the first frame update
     void Start()
     {
         if (player.active == false)
         {
             // Disable joe jeff because he keeps running into the player. Fix this please
-            Destroy(gameObject);
+            //Destroy(gameObject);
             player = GameObject.Find("3dPlayerObjs");
         }
-        joeJeff = GetComponent<NavMeshAgent>();
-        offset = new Vector3(UnityEngine.Random.Range(0.0f, 0.5f), 0, UnityEngine.Random.Range(0.0f, 0.5f));
+        setInitialReferences();
+        //joeJeff = GetComponent<NavMeshAgent>();
+        //offset = new Vector3(UnityEngine.Random.Range(0.0f, 0.5f), 0, UnityEngine.Random.Range(0.0f, 0.5f));
     }
-    private void FixedUpdate()
+
+    void Update()
     {
-            
-            joeJeff.destination = player.transform.position + offset;
+        checkIfPlayerInRange();
+    }
+
+    void setInitialReferences()
+    {
+        myTransform = transform;
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+        checkRate = Random.Range(0.8f, 1.2f);   //how often to check for player
+    }
+    void checkIfPlayerInRange()
+    {
+        if(Time.deltaTime > nextCheck && myNavMeshAgent == true)
+        {
+            nextCheck = Time.deltaTime + checkRate;
+            hitColliders = Physics.OverlapSphere(myTransform.position, detectionRadius, detectionLayer);
+            //found the player
+            if(hitColliders.Length >0)
+            {
+                myNavMeshAgent.SetDestination(hitColliders[0].transform.position);
+            }
+        }
     }
 }
