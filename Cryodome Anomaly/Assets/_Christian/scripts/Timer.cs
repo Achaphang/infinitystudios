0272f44e9117d60c2951e5e8f15b6d6cef50f346
oceanwhit <oceanwhit@gmail.com>
2020-03-28 15:18:18 -0700

    Add Timer script

---
+++ Cryodome Anomaly/Assets/_Christian/scripts/Timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("=========== IN Timer =========");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
