630ead0327809e09434b19224b7ecd885f070164
oceanwhit <oceanwhit@gmail.com>
2020-03-28 21:35:50 -0700

    Change timer formatting

--- Cryodome Anomaly/Assets/_Christian/scripts/Timer.cs
+++ Cryodome Anomaly/Assets/_Christian/scripts/Timer.cs
@@ -8,20 +8,25 @@ public class Timer : MonoBehaviour
     public Text timerText;
     float seconds = 0.0f;
     int minutes = 0;
+    string time;
 
     // Update is called once per frame
     void Update()
     {
+        time = "";
         seconds += Time.deltaTime;
         if (seconds >= 60.0f) {
             minutes++;
             seconds -= 60.0f;
         }
         if (minutes < 10) {
-            timerText.text = "0" + minutes.ToString() + ":" + seconds.ToString("F2");
-
-        } else {
-            timerText.text = minutes.ToString() + ":" + seconds.ToString("F2");
+            time += "0";
         }
+        time += minutes.ToString() + ":";
+        if (seconds < 10) {
+            time += "0";
+        }
+        time += seconds.ToString("F2");
+        timerText.text = time;
     }
 }
