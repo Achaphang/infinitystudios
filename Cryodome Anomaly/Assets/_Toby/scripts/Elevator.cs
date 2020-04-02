5211a2644d1726c551453d6b6907cbaaaf2e3293
DevDrako <45469673+DevDrako@users.noreply.github.com>
2020-04-01 21:38:16 -0700

    Added Environment Features
    Added Elevator (not functional yet)
    Continued adding room and corridor tiles

---
+++ Cryodome Anomaly/Assets/_Toby/scripts/Elevator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    bool upperLevel = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray r = FindObjectOfType<Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit ray;
+
+        //Checks if the player is near the door so it can open
+        if(true)
+        {
+
+        }
+
+        //Checks if the buttons are clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(r, out ray) && ray.transform.gameObject.name == "Up Button")
+            {
+                upperLevel = true;
+                Debug.Log("Registered Up Click");
+
+                while (transform.position != new Vector3(-0.052f, 3, 0))
+                {
+                    transform.Translate(new Vector3(0, 0.02f, 0));
+                }
+
+            }
+
+            if (Physics.Raycast(r, out ray) && ray.transform.gameObject.name == "Down Button")
+            {
+                upperLevel = false;
+                Debug.Log("Registered Down Click");
+
+                transform.position = new Vector3(-0.052f, 0, 0);
+            }
+        }
+    }
+}
---
+++ Cryodome Anomaly/Assets/_Toby/scripts/ElevatorDoorOpen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorDoorOpen : MonoBehaviour
+{
+    public GameObject door;
+    public bool isOpened = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.transform.gameObject.name);
+
+        if (other.transform.gameObject.name == "3dPlayerObjs" && isOpened == false)
+        {
+            Debug.Log("I see you");
+            StartCoroutine("DoorOpener");
+
+            isOpened = true;
+        }
+    }
+
+    IEnumerator DoorOpener()
+    {
+        Debug.Log("Coroutine Started");
+        Debug.Log(door.transform.position);
+        while (door.transform.position.y < 4.45f)
+        {
+            Debug.Log("Position changing");
+            door.transform.Translate(new Vector3(0, 0.02f, 0));
+            yield return new WaitForSeconds(0.01f);
+        }
+        Debug.Log(door.transform.position);
+        Debug.Log("Coroutine About to End");
+        yield return new WaitForSeconds(2);
+        isOpened = false;
+    }
+
+}
