d58d31e2f7bca519c627694edcc3e9270564b359
Yamist <8bityamist@gmail.com>
2020-04-06 23:35:37 -0700

    Slightly improved performance of teleport radius

--- Cryodome Anomaly/Assets/_Damien/Scripts/teleportRadius.cs
+++ Cryodome Anomaly/Assets/_Damien/Scripts/teleportRadius.cs
@@ -15,21 +15,20 @@ public class teleportRadius : MonoBehaviour
     {
         xVal = 0.08f;
         zVal = 0.08f;
-        iteration = 1;
         defaultRad = new Vector3(.2f, 0.01f, .2f);
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if ((SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Teleport").GetState(SteamVR_Input_Sources.Any)) && iteration <= 100) {
-            gameObject.transform.localScale = new Vector3(.2f + xVal * iteration, 1, .2f + zVal * iteration);
-            //temp.transform.localScale = new Vector3(.2f + xVal * iteration, 0.5f, .2f + zVal * iteration);
-            iteration += 1;
+            gameObject.transform.localScale = new Vector3(.2f + xVal, 1, .2f + zVal);
+            xVal += .08f;
+            zVal += .08f;
         }else if(!(SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Teleport").GetState(SteamVR_Input_Sources.Any))) {
             gameObject.transform.localScale = defaultRad;
-            //temp.transform.localScale = new Vector3(.2f + xVal * iteration, 0.5f, .2f + zVal * iteration);
-            iteration = 1;
+            xVal = .08f;
+            zVal = .08f;
         }
     }
 }
