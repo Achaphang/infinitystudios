98b6d640bac07a7bea8a0d775bf31f5e8c780738
Hunter Hawkins <hunterdhawkins@gmail.com>
2020-04-09 22:35:13 -0700

    Dancing until death

---
+++ Cryodome Anomaly/Assets/_Hunter/Scripts/DanceScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceScript : MonoBehaviour
+{
+    private Animator anim;
+    public float timer = 0.0f;
+    public int seconds;
+    public bool keepTiming = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+        if (timer > 10.0f)
+        {
+            anim.SetBool("NewDance", true);
+            ResetTimer();
+
+            
+        }
+        else
+        {
+            anim.SetBool("NewDance", false);
+        }
+        
+    }
+
+    public void Timer()
+    {
+        // seconds
+        timer += Time.deltaTime;
+        // turn float seonds to int
+        seconds = (int)(timer % 60);
+        //print(seconds);
+    }
+
+
+    public void ResetTimer()
+    {
+        // seconds
+        timer += Time.deltaTime;
+        // turn float seonds to int
+        seconds = (int)(timer % 60);
+        print(seconds);
+
+        if (seconds > 4)
+        {
+            // both code runs the same
+            //timer = Time.deltaTime;
+            timer = 0.0f;
+        }
+    }
+}
