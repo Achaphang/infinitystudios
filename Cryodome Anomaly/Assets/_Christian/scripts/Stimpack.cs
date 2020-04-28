d1406665b75015a0a8c0b63aed3a033e1e715311
oceanwhit <oceanwhit@gmail.com>
2020-04-27 19:39:45 -0700

    Stimpack.cs

---
+++ Cryodome Anomaly/Assets/_Christian/scripts/Stimpack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stimpack : MonoBehaviour
+{
+    public Transform stimpackPrefab;
+    
+    void OnCollisionEnter(Collision col) {
+        if (col.collider.name == "3dPlayerObjs") {
+            Debug.Log("Collided! now 2");
+        }
+    }
+}
