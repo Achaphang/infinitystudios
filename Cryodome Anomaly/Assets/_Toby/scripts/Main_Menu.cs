c3fcdb9a8744fb31fec103171d3efecc43b890e5
DevDrako <45469673+DevDrako@users.noreply.github.com>
2020-04-30 22:03:36 -0700

    Commented Code

--- Cryodome Anomaly/Assets/_Toby/scripts/Main_Menu.cs
+++ Cryodome Anomaly/Assets/_Toby/scripts/Main_Menu.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 
 public class Main_Menu : Menu
 {
-    public GameObject chooseInput;
-    public GameObject startButton;
-    public GameObject loadingScreen;
+    public GameObject chooseInput;  //The menu that allows the user to choose their input mode
+    public GameObject startButton;  //Enters the rest of the main menu
+    public GameObject loadingScreen;  //Shows while the main scene is loading
 
     private new void Start()
     {
@@ -16,30 +16,34 @@ public class Main_Menu : Menu
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
 
-        base.Start();
+        base.Start();  //Execute code from Parent Class
     }
 
+    //Executes when the startButton is clicked
     public void OnPlayGameClick()
     {
+        //Hide startButton and show the mainPanel
         mainPanel.SetActive(true);
         startButton.SetActive(false);
-        buttonSound.Play();
+        buttonSound.Play();  //Play the fun clicky button sound
     }
 
+    //Activates game in Normal Mode
     public void OnNormalModeClick()
     {
-        buttonSound.Play();
+        buttonSound.Play();  //Play the fun clicky button sound
 
-        PlayerPrefs.SetInt("Mode", 1);   //Remember PlayerPref for Scene Change
+        PlayerPrefs.SetInt("Mode", 1);  //Remember PlayerPref for Scene Change
 
         //Hide mainMenu Menu and Activate chooseInput Menu
         mainPanel.SetActive(false);
         chooseInput.SetActive(true);
     }
 
+    //Activates game in BC Mode
     public void OnBCModeClick()
     {
-        buttonSound.Play();
+        buttonSound.Play();  //Play the fun clicky button sound
 
         PlayerPrefs.SetInt("Mode", 2);   //Remember PlayerPref for Scene Change
 
@@ -48,19 +52,20 @@ public class Main_Menu : Menu
         chooseInput.SetActive(true);
     }
 
+    //Shows Leaderboard
     public void OnLeaderboardClick()
     {
-        buttonSound.Play();
+        buttonSound.Play();  //Play the fun clicky button sound
 
-        //Waiting for database to exist
+        //Waiting for "database" to exist
     }
     
-
+    //Starts the game with the VR PlayerPref activated
     public void OnVirtualRealityClick()
     {
-        buttonSound.Play();
-        loadingScreen.SetActive(true);
-        musicVolume.mute = true;
+        buttonSound.Play();  //Play the fun clicky button sound
+        loadingScreen.SetActive(true);  //Show loading screen
+        musicVolume.mute = true;  //Stop main menu music
 
         PlayerPrefs.SetInt("Input", 1);   //Remember PlayerPref for Scene Change
 
@@ -68,11 +73,12 @@ public class Main_Menu : Menu
         StartCoroutine("LoadYourAsyncScene");
     }
 
+    //Starts the game with the Keyboard/Mouse activated
     public void OnKeyboardClick()
     {
-        buttonSound.Play();
-        loadingScreen.SetActive(true);
-        musicVolume.mute = true;
+        buttonSound.Play();  //Play the fun clicky button sound
+        loadingScreen.SetActive(true);  //Show loading screen
+        musicVolume.mute = true;  //Stop main menu music
 
         PlayerPrefs.SetInt("Input", 2);   //Remember PlayerPref for Scene Change
 
@@ -80,16 +86,18 @@ public class Main_Menu : Menu
         StartCoroutine("LoadYourAsyncScene");
     }
 
+    //Exits the game and closes the application
     public override void OnExitClick()
     {
-        buttonSound.Play();
+        buttonSound.Play();  //Play the fun clicky button sound
 
         Application.Quit();   //Quit Application
     }
 
+    //Returns to the mainPanel from the settingsPanel
     public override void OnBackClick()
     {
-        buttonSound.Play();
+        buttonSound.Play();  //Play the fun clicky button sound
 
         //Hide chooseInput Menu and Reactivate mainMenu
         mainPanel.SetActive(true);
--- Cryodome Anomaly/Assets/_Toby/scripts/Menu.cs
+++ Cryodome Anomaly/Assets/_Toby/scripts/Menu.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 
 public abstract class Menu : MonoBehaviour
 {
-    public GameObject mainPanel;
-    public GameObject settingsPanel;
-    public Slider masterSlider;
-    public Slider musicSlider;
-    public AudioSource musicVolume;
-    public AudioSource buttonSound;
-    public string sceneToLoad;
+    public GameObject mainPanel;  //The current menu panel
+    public GameObject settingsPanel;  //The current settings panel
+    public Slider masterSlider;  //Adjusts the master volume
+    public Slider musicSlider;  //Adjusts the music volume
+    public AudioSource musicVolume;  //The music source
+    public AudioSource buttonSound;  //The fun clicky button sound source
+    public string sceneToLoad;  //Stores the next scene to load if the user exits the current menu
 
     // Start is called before the first frame update
     protected virtual void Start()
@@ -24,6 +24,7 @@ public abstract class Menu : MonoBehaviour
 
     public void OnSettingsClick()
     {
+        //Play the fun clicky button sound
         buttonSound.Play();
 
         //Hide mainMenu Menu and Activate settingsMenu Menu
@@ -31,26 +32,30 @@ public abstract class Menu : MonoBehaviour
         settingsPanel.SetActive(true);
     }
 
-    public abstract void OnExitClick();
-
     public void MasterVolumeSlider()
     {
+        //Attribute Master Volume to the masterSlider
         PlayerPrefs.SetFloat("Master Volume", masterSlider.value);
 
+        //Set the slider
         AudioListener.volume = masterSlider.value;
     }
 
     public void MusicVolumeSlider()
     {
+        //Attribute Music Volume to the musicSlider
         PlayerPrefs.SetFloat("Music Volume", musicSlider.value);
 
+        //Set the slider
         musicVolume.volume = musicSlider.value;
     }
 
     IEnumerator LoadYourAsyncScene()
     {
+        //Provide time for beep sounds and loading screens to play
         yield return new WaitForSeconds(1);
 
+        //Loads into the next relevant scene
         AsyncOperation load = SceneManager.LoadSceneAsync(sceneToLoad);
 
         // Wait until the asynchronous scene fully loads
@@ -60,5 +65,10 @@ public abstract class Menu : MonoBehaviour
         }
     }
 
+    //Operates code that executes when the button to completely exit the menu is clicked
+    public abstract void OnExitClick();
+
+    //Operates code that executes when the button to head back to the main panel (after
+    //returning from the settings panel) is clicked
     public abstract void OnBackClick();
 }
